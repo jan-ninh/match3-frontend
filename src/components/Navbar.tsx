6e62d23129033e0822490331992fce92e84a6d87
Elsa Ebrahimi <teaserpluss@gmail.com>
2026-01-29 23:35:47 +0330

    Add Navar & icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,43 @@
 ﻿import { Link } from 'react-router-dom';
 
+type NavItem = {
+  label: string;
+  icon: string;
+  to?: string;
+  clickable: boolean;
+};
+
+const navItems: NavItem[] = [
+  { label: 'Home', icon: '/icons/home.svg', to: '/game-map', clickable: true },
+  { label: 'Login', icon: '/icons/login.svg', to: '/game-map/login', clickable: true },
+  { label: 'Logout', icon: '/icons/logout.svg', to: '/game-map/logout', clickable: true },
+  { label: 'Hearts', icon: '/icons/heart.svg', clickable: false },
+  { label: 'Leaderboard', icon: '/icons/cup.svg', to: '/game-map/leaderboard', clickable: true },
+  { label: 'Profile', icon: '/icons/user.svg', to: '/game-map/profile', clickable: true },
+  { label: 'Settings', icon: '/icons/setting.svg', to: '/game-map/setting', clickable: true },
+];
+
 const Navbar = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/game-map">Home</Link>
+    <nav className="flex justify-between p-8">
+      <h1>Match-3</h1>
+      <ul className="flex justify-end space-x-6 items-center ">
+        {navItems.map(({ label, icon, to, clickable }) => (
+          <li key={label}>
+            {clickable && to ? (
+              <Link to={to} className="flex flex-col items-center hover:bg-blue-300 transition">
+                <img src={icon} alt={label} className="w-9 h-9" />
+              </Link>
+            ) : (
+              <div className="flex flex-col items-center cursor-default">
+                <img src={icon} alt={label} className="w-9 h-9" />
+              </div>
+            )}
           </li>
-          <li>
-            <Link to="/game-map/login">log in</Link>
-          </li>
-          <li>
-            <Link to="/game-map/logout">log out</Link>
-          </li>
-          <li>
-            <Link to="/game-map">hearts</Link>
-          </li>
-          <li>
-            <Link to="/game-map/profile">profile</Link>
-          </li>
-          <li>
-            <Link to="/game-map/leaderboard">leaderboard</Link>
-          </li>
-          <li>
-            <Link to="/game-map/setting">setting</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+        ))}
+      </ul>
+    </nav>
   );
 };
+
 export default Navbar;
