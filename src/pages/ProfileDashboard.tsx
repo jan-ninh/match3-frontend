332cebbabe2fc85db3db4d37a48f8ad20b0bd0c2
Elsa Ebrahimi <teaserpluss@gmail.com>
2026-02-02 16:49:17 +0330

    add Avatar & Dashboard

--- src/pages/ProfileDashboard.tsx
+++ src/pages/ProfileDashboard.tsx
@@ -1,18 +1,25 @@
-import { Avatar, Badge } from '@/components';
+import { ProfileHeader, StatsGrid, ProgressBar, BadgeGrid, Avatar } from '@/components';
+import badges from '../data/badges';
 
-const ProfilePage = () => {
-  return (
-    <>
-      <div>
-        <Avatar />
-        <p>Name</p>
-      </div>
-      <div>info of user game</div>
-      <div>progressbar</div>
-      <div>
-        <Badge />
-      </div>
-    </>
-  );
-};
-export default ProfilePage;
+const ProfileDashboard = () => (
+  <div className="m-4 w-lg mx-auto flex flex-col items-center space-y-4">
+    <ProfileHeader username="Elsa" level={12}>
+      <Avatar size={124} />
+    </ProfileHeader>
+
+    <StatsGrid
+      stats={[
+        { label: 'Wins', value: 24 },
+        { label: 'Losses', value: 8 },
+        { label: 'XP', value: '1,240' },
+        { label: 'Rank', value: 'Gold' },
+      ]}
+    />
+
+    <ProgressBar percent={65} />
+
+    <BadgeGrid badges={badges} />
+  </div>
+);
+
+export default ProfileDashboard;
