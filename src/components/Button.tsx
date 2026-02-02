0b8a1c6c384f6d014bd1e363abd4d7579a336f5e
Elsa Ebrahimi <teaserpluss@gmail.com>
2026-02-02 21:21:38 +0330

    add type button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,11 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string;
 };
 
-export default function Button({ label, className = '', ...props }: ButtonProps) {
+export default function Button({ label, className = '', type = 'button', ...props }: ButtonProps) {
   const baseClasses = 'w-48 md:w-60 py-2 rounded text-white bg-neutral-800 hover:bg-neutral-500 transition-colors duration-200';
 
   return (
-    <button className={`${baseClasses} ${className}`} {...props}>
+    <button type={type} className={`${baseClasses} ${className}`} {...props}>
       {label}
     </button>
   );
