f757ce3e1f2fb17f7f3b6ccfe0d210e04a76ec3b
Elsa <teaserpluss@gmail.com>
2026-02-02 13:57:36 +0100

    Add profile dashboard (#12)
    * first layout Dashboard
    
    * add Dashboard Layout
    
    * optimizing code
    
    * small tailwind change

---
+++ src/data/badges.ts
@@ -0,0 +1,40 @@
+const badges = [
+  {
+    id: 'bronze',
+    label: 'Bronze Shield',
+    icon: '/icons/badge.svg',
+    unlocked: true,
+  },
+  {
+    id: 'silver',
+    label: 'Silver Shield',
+    icon: '/icons/badge.svg',
+    unlocked: false,
+  },
+  {
+    id: 'gold',
+    label: 'Gold Shield',
+    icon: '/icons/badge.svg',
+    unlocked: false,
+  },
+  {
+    id: 'platinum',
+    label: 'Platinum Shield',
+    icon: '/icons/badge.svg',
+    unlocked: false,
+  },
+  {
+    id: 'diamond',
+    label: 'Diamond Shield',
+    icon: '/icons/badge.svg',
+    unlocked: false,
+  },
+  {
+    id: 'special',
+    label: 'Special Badge',
+    icon: '/icons/badge.svg',
+    unlocked: true,
+  },
+];
+
+export default badges;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,4 @@ export { default as GameplayPage } from './GameplayPage';
 export { default as GameStartPage } from './GameStartPage';
 export { default as LevelMapPage } from './LevelMapPage';
 export { default as LeaderboardPage } from './LeaderboardPage';
-export { default as ProfilePage } from './ProfilePage';
+export { default as ProfileDashboard } from './ProfileDashboard';
