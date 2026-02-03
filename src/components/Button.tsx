8be18c2097acd9e7d323cec170c8b8a0f9ce0a58
jan-ninh <janninh@gmx.de>
2026-02-03 12:42:55 +0100

    chore(build): clean up POM

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-﻿import type { ButtonHTMLAttributes } from 'react';
+import type { ButtonHTMLAttributes } from 'react';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label: string;
