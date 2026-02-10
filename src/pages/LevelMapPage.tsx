baf962940c652f199069cda62a0aae8e7ca16ed1
jan-ninh <janninh@gmx.de>
2026-02-10 18:29:45 +0100

    feature: choosePower-overlay triggers after winning

--- src/pages/LevelMapPage.tsx
+++ src/pages/LevelMapPage.tsx
@@ -2,19 +2,16 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Navbar, LevelGrid, CyberTitle } from '@/components';
 import { getProgress } from '@/services/progress/progressActions';
-import { useOverlays } from '@/features/overlays';
 import type { LevelId, Progress } from '@/services/progress/ProgressStore';
 import { apiProfile } from '@/api/user';
 import { useAuth } from '@/context/AuthContext';
-import type { Powers, UserProfile } from '@/types';
-import { usePowers } from '@/context/PowerContext';
+import type { UserProfile } from '@/types';
 
 export default function LevelMapPage() {
   const navigate = useNavigate();
   const [progress, setProgress] = useState<Progress | null>(null);
-  const { openPowerChoice } = useOverlays();
+
   const { user } = useAuth();
-  const { setFromBackendAndSelect } = usePowers();
 
   const highestCompletedRef = useRef(0);
 
@@ -49,24 +46,28 @@ export default function LevelMapPage() {
   }, [user?.id]);
 
   const onSelect = (level: LevelId) => {
-    openPowerChoice({
-      title: 'Choose your Power!',
-      onChoose: async (powerId) => {
-        const fallback: Powers = { bomb: 0, rocket: 0, extraTime: 0 };
-        try {
-          if (user?.id) {
-            const profile = await apiProfile(user.id);
-            setFromBackendAndSelect(profile.powers ?? fallback, powerId);
-          } else {
-            setFromBackendAndSelect(fallback, powerId);
-          }
-        } catch {
-          setFromBackendAndSelect(fallback, powerId);
-        }
-        navigate(`/game-map/play-game?level=${level}`);
-      },
-    });
+    navigate(`/game-map/play-game?level=${level}`);
   };
+
+  // const onSelect = (level: LevelId) => {
+  //   openPowerChoice({
+  //     title: 'Choose your Power!',
+  //     onChoose: async (powerId) => {
+  //       const fallback: Powers = { bomb: 0, rocket: 0, extraTime: 0 };
+  //       try {
+  //         if (user?.id) {
+  //           const profile = await apiProfile(user.id);
+  //           setFromBackendAndSelect(profile.powers ?? fallback, powerId);
+  //         } else {
+  //           setFromBackendAndSelect(fallback, powerId);
+  //         }
+  //       } catch {
+  //         setFromBackendAndSelect(fallback, powerId);
+  //       }
+  //       navigate(`/game-map/play-game?level=${level}`);
+  //     },
+  //   });
+  // };
 
   if (!progress) return <div className="p-6">Loading levels…</div>;
 
